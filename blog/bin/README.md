900c24052b80afe6d93c305db3e3782445d16c82
jkb <skjin20800@naver.com>
2021-03-16 09:02:48 +0900

    2021-03-16 한교

---
+++ blog/bin/README.md
@@ -0,0 +1,14 @@
+# 스프링부트 블로그 프로젝트 V1
+
+## 의존성
+- Spring Web
+- ...
+- ...
+
+## DB 설정
+
+```sql
+create user 'pos'@'%' identified by 'pos1234';
+GRANT ALL PRIVILEGES ON *.* TO 'pos'@'%';
+create database pos;
+```
--- jwtex1/src/main/java/com/cos/jwtex1/web/RestApiController.java
+++ jwtex1/src/main/java/com/cos/jwtex1/web/RestApiController.java
@@ -1,7 +1,6 @@
 package com.cos.jwtex1.web;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
